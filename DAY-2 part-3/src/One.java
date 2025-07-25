a0910e873e2eed343a7c345c1987c18caa2afe89
Chiranjeevi006 <chiranjeevi402d@gmail.com>
2025-07-25 14:53:31 +0530

    Renamed

---
+++ DAY-2 part-3/src/One.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class One {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the two Strings in Binary form:");
+        String s1=scan.nextLine();
+        String s2=scan.nextLine();
+        int carry=0;
+        int n1=s1.length();
+        int n2=s2.length();
+   }
+}
